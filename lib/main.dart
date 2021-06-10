d936c15fb3b23d1bb55db16f96bba06ab5318f23
jamil-qn <jamil.qolinezhad@gmail.com>
2021-06-10 21:07:34 +0430

    add some basic style for look better

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyHomePage extends StatelessWidget {
   final List<Transaction> transactions = [
     Transaction(
         id: 't1', title: 'new Shoes', amount: 70.31, date: DateTime.now()),
-    Transaction(id: 't2', title: 'ice cream', amount: 2, date: DateTime.now()),
+    Transaction(id: 't2', title: 'ice cream', amount: 2.876, date: DateTime.now()),
   ];
   @override
   Widget build(BuildContext context) {
@@ -42,11 +42,32 @@ class MyHomePage extends StatelessWidget {
                 return Card(
                   child: Row(
                     children: [
-                      Container(child: Text(tx.amount.toString())),
+                      Container(
+                          margin: EdgeInsets.symmetric(
+                              vertical: 10, horizontal: 15),
+                          decoration: BoxDecoration(
+                              border:
+                                  Border.all(color: Colors.purple, width: 2)),
+                          padding: EdgeInsets.all(10),
+                          child: Text(
+                            tx.amount.toString(),
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Colors.purple),
+                          )),
                       Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text(tx.title),
-                          Text(tx.date.toString()),
+                          Text(
+                            tx.title,
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            tx.date.toString(),
+                            style: TextStyle(color: Colors.grey),
+                          ),
                         ],
                       )
                     ],
