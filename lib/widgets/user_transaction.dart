b2ed22cfa382b89b079dbc2202e18884b64e0f57
jamil-qn <jamil.qolinezhad@gmail.com>
2021-06-11 10:55:59 +0430

    get user traction and add new transaction card to list when user press add transaction button

---
+++ lib/widgets/user_transaction.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import './transaction_list.dart';
+import './new_transaction.dart';
+import '../models/transaction.dart';
+
+class UserTransaction extends StatefulWidget {
+  @override
+  _UserTransactionState createState() => _UserTransactionState();
+}
+
+class _UserTransactionState extends State<UserTransaction> {
+  final List<Transaction> _usertransactions = [
+    Transaction(
+        id: 't1', title: 'new Shoes', amount: 70.31, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'ice cream', amount: 2.876, date: DateTime.now()),
+  ];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+        title: txTitle,
+        amount: txAmount,
+        date: DateTime.now(),
+        id: DateTime.now().toString());
+
+        setState(() {
+          _usertransactions.add(newTx);
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [NewTransaction(_addNewTransaction), TransactionList(_usertransactions)],
+    );
+  }
+}
