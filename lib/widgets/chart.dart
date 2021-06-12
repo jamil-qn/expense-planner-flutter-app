fe87d7a16d6d95504cf93fa659b24551a9bca3d4
jamil-qn <jamil.qolinezhad@gmail.com>
2021-06-12 09:41:09 +0430

    add chart bar for show expense in each week day

---
+++ lib/widgets/chart.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../models/transaction.dart';
+import './chart_bar.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+  Chart(this.recentTransactions);
+
+  List<Map<String, Object>> get groupTransactionValues {
+    return List.generate(7, (index) {
+      final weekDay =
+          DateTime.now().subtract(Duration(days: index)); //find week day
+      double totalSun = 0.0;
+
+      for (var i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].date.day == weekDay.day &&
+            recentTransactions[i].date.month == weekDay.month &&
+            recentTransactions[i].date.year == weekDay.year) {
+          totalSun += recentTransactions[i].amount;
+        }
+      }
+
+      print(DateFormat.E().format(weekDay));
+      print(totalSun);
+
+      return {
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
+        'amount': totalSun
+      };
+    });
+  }
+
+  double get totalSpending {
+    return groupTransactionValues.fold(0.0, (sun, item) {
+      return sun + (item['amount'] as double);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print(groupTransactionValues);
+    return Card(
+      elevation: 5,
+      margin: EdgeInsets.all(20),
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupTransactionValues.map((data) {
+            return Flexible(
+              fit: FlexFit.tight,
+                        child: ChartBar(
+                  data['day'] as String,
+                  data['amount'] as double,
+                  totalSpending == 0.0
+                      ? 0.0
+                      : (data['amount'] as double) / totalSpending),
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}
